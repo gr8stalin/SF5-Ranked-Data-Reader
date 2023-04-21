b469a99b667e3879c585a1aa34211ee6ad0d8b08
gr8stalin <gr8stalin@gmail.com>
2023-04-20 22:35:04 -0400

    Add some comments and print statements, update readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# SF5-Ranked-Data-Reader
-Python script for parsing the Excel sheet I used to track my ranked progress to Diamond rank in SF5
+# SF5 Ranked Data Reader
+Python script for parsing the Excel sheet I used to track my ranked progress to Diamond rank in SF5.
+
+This script takes in my Excel sheet and spits out the data as a .json file, which I intend to use as a "database"
+for an accompanying data visualization project.
+
+Part of this project is just about reviewing some of my Python knowledge and filling in some gaps in my knowledge.
--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import dateutil.parser
 
 
 def parse_data_from_workbook():
+    # Hardcode the filepath since I just need to run the script
     ranked_data_workbook = openpyxl.load_workbook("/home/gr8stalin/Documents/jiveGrind.xlsx")
+    # Exclude any of the extra empty sheets I added as a buffer
     sheets = [x for x in ranked_data_workbook.sheetnames if "New" not in x]
     sessions = []
     for sheet in sheets:
@@ -14,6 +16,7 @@ def parse_data_from_workbook():
         matches = []
         for row in worksheet.iter_rows(min_row=2, max_col=4, values_only=True):
             if not all(row):
+                print(f"Encountered empty row in sheet '{sheet}', reached end of matches")
                 break
             else:
                 matches.append(create_ranked_match(row))
@@ -40,5 +43,7 @@ def create_ranked_session(worksheet, matches):
 if __name__ == '__main__':
     all_ranked_sessions = parse_data_from_workbook()
     ranked_sessions_json = json.dumps(all_ranked_sessions, cls=RankedSessionEncoder)
+    # Hardcode the filepath here as well
     with open("/home/gr8stalin/Documents/ranked_data.json", 'w', encoding='utf-8') as writer:
+        print("Dumping serialized ranked sessions")
         json.dump(all_ranked_sessions, writer, ensure_ascii=False, cls=RankedSessionEncoder)
